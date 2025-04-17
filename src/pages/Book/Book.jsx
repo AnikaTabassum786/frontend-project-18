25f30f4cbe4ec074f94964c08e6181040cc7595c
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-04-17 22:24:14 +0600

    Polishing the product card and adding React Icons

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-
+import { FaRegStar } from "react-icons/fa";
 const Book = ({singleBook}) => {
-    const{bookName,author,image,rating} = singleBook
+    const{bookName,author,image,rating,category} = singleBook
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img className='w-40 h-40 py-4'
+        <div className="card bg-base-100 w-96 shadow-sm border p-6">
+  <figure className='p-6 bg-gray-100 w-2/3 mx-auto'>
+    <img className='h-[166px]'
       src={image}
       alt="Book" />
   </figure>
   <div className="card-body">
     <h2 className="card-title">
-      <p>{bookName}</p>
+      {bookName}
       <div className="badge badge-secondary">NEW</div>
     </h2>
     <p>By: {author}</p>
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">{}</div>
-      <div className="badge badge-outline">{rating}</div>
+      <div className="badge">{category}</div>
+      <div className="badge ">{rating} <FaRegStar></FaRegStar></div>
     </div>
   </div>
 </div>
